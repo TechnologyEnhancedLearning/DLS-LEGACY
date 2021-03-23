9bf6bca55abde19a0381b1b7aa1bd32b69737aa7
kevwhitt-hee <kevin.whittaker@hee.nhs.uk>
2021-03-23 12:04:16 +0000

    #38 Adds nonav master page and uses in landing pages
    Implemented in pricing, find your centre and learning pages but can easily be extended to any others.

--- ITSP-TrackingSystemRefactor/pricing.aspx.vb
+++ ITSP-TrackingSystemRefactor/pricing.aspx.vb
@@ -1,6 +1,12 @@
 ﻿Public Class pricing
     Inherits System.Web.UI.Page
-
+    Private Sub me_PreInit(sender As Object, e As EventArgs) Handles Me.PreInit
+        If Not Page.Request.Item("nonav") Is Nothing Then
+            MasterPageFile = "~/nonav.Master"
+        Else
+            MasterPageFile = "~/Landing.Master"
+        End If
+    End Sub
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
 
     End Sub
