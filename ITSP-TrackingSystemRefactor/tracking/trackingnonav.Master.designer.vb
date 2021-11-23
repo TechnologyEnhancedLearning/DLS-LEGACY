6f6a936765104601f5f3405c9272138414904e52
kevwhitt-hee <kevin.whittaker@hee.nhs.uk>
2021-11-23 15:11:21 +0000

    #100 Adds tracking nonav master and applies to Tickets.aspx
    With custom code to tweak close ticket redirect URL etc

--- ITSP-TrackingSystemRefactor/tracking/trackingnonav.Master.designer.vb
+++ ITSP-TrackingSystemRefactor/tracking/trackingnonav.Master.designer.vb
@@ -11,7 +11,7 @@ Option Strict On
 Option Explicit On
 
 
-Partial Public Class nonav
+Partial Public Class trackingnonav
 
     '''<summary>
     '''form1 control.
@@ -21,15 +21,6 @@ Partial Public Class nonav
     '''To modify move field declaration from designer file to code-behind file.
     '''</remarks>
     Protected WithEvents form1 As Global.System.Web.UI.HtmlControls.HtmlForm
-
-    '''<summary>
-    '''ScriptManager1 control.
-    '''</summary>
-    '''<remarks>
-    '''Auto-generated field.
-    '''To modify move field declaration from designer file to code-behind file.
-    '''</remarks>
-    Protected WithEvents ScriptManager1 As Global.System.Web.UI.ScriptManager
 
     '''<summary>
     '''SkipToContentLink control.
