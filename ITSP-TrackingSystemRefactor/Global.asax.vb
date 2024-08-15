f53d39c7ee12e1e1d00848607763e01938031185
kevwhitt-hee <kevin.whittaker@hee.nhs.uk>
2024-08-15 17:10:03 +0100

    TD-4365 Adds redirects to V2 app pages from all relevant page requests in the legacy app

--- ITSP-TrackingSystemRefactor/Global.asax.vb
+++ ITSP-TrackingSystemRefactor/Global.asax.vb
@@ -12,6 +12,7 @@ Public Class Global_asax
     End Sub
     Sub Application_BeginRequest(sender As Object, e As EventArgs)
         Dim uriObject As String = HttpContext.Current.Request.Url.OriginalString
+        Dim baseUrl As String = CCommon.GetConfigString("V2AppBaseUrl")
         'app.Context.Request.Url.OriginalString
         If uriObject.ToString.Contains("/learnsco") Or uriObject.ToString.Contains("/sco") Or uriObject.ToString.Contains("/CMSContent") Or uriObject.ToString.Contains("/eitslm") Or uriObject.ToString.Contains("/finalise") Or uriObject.ToString.Contains("/errorAccess") Or uriObject.ToString.Contains("/pricing") Or uriObject.ToString.Contains("/findyourcentre") Or uriObject.ToString.Contains("/Learning") Then
             Me.Response.Headers("X-FRAME-OPTIONS") = "ALLOWALL"
@@ -21,6 +22,63 @@ Public Class Global_asax
             Me.Response.Headers.Add("X-FRAME-OPTIONS", "ALLOW-FROM https://localhost:44363/")
             Me.Response.Headers.Add("X-FRAME-OPTIONS", "ALLOW-FROM https://hee-dls-test.softwire.com/")
             Me.Response.Headers.Add("X-FRAME-OPTIONS", "ALLOW-FROM https://www.dls.nhs.uk/")
+        End If
+        If uriObject.Contains("tracking/approvedelegates") Then
+            Response.RedirectPermanent(baseUrl & "TrackingSystem/Delegates/Approve")
+        End If
+        If uriObject.Contains("tracking/centredetails") Then
+            Response.RedirectPermanent(baseUrl & "TrackingSystem/Centre/Configuration")
+        End If
+        If uriObject.Contains("tracking/centrelogins") Then
+            Response.RedirectPermanent(baseUrl & "TrackingSystem/Centre/Administrators")
+        End If
+        If uriObject.Contains("tracking/coursedelegates") Then
+            Response.RedirectPermanent(baseUrl & "TrackingSystem/Delegates/Activities")
+        End If
+        If uriObject.Contains("tracking/coursesetup") Then
+            Response.RedirectPermanent(baseUrl & "TrackingSystem/CourseSetup")
+        End If
+        If uriObject.Contains("tracking/dashboard") Then
+            Response.RedirectPermanent(baseUrl & "TrackingSystem/Centre/Dashboard")
+        End If
+        If uriObject.Contains("tracking/Default") Then
+            Response.RedirectPermanent(baseUrl & "Login")
+        End If
+        If uriObject.Contains("tracking/delegategroups") Then
+            Response.RedirectPermanent(baseUrl & "TrackingSystem/Delegates/Groups")
+        End If
+        If uriObject.Contains("tracking/delegates") Then
+            Response.RedirectPermanent(baseUrl & "TrackingSystem/Delegates/All")
+        End If
+        If uriObject.Contains("tracking/download") Or uriObject.Contains("tracking/resources") Then
+            Response.RedirectPermanent(baseUrl & "TrackingSystem/Resources")
+        End If
+        If uriObject.Contains("tracking/faqs") Then
+            Response.RedirectPermanent(baseUrl & "TrackingSystem/Support/FAQs")
+        End If
+        If uriObject.Contains("tracking/reports") Then
+            Response.RedirectPermanent(baseUrl & "TrackingSystem/Centre/Reports/Courses")
+        End If
+        If uriObject.Contains("tracking/supervise") Then
+            Response.RedirectPermanent(baseUrl & "Supervisor")
+        End If
+        If uriObject.Contains("tracking/support") Then
+            Response.RedirectPermanent(baseUrl & "TrackingSystem/Support")
+        End If
+        If uriObject.Contains("tracking/centredetails") Then
+            Response.RedirectPermanent(baseUrl & "TrackingSystem/Centre/Configuration")
+        End If
+        If uriObject.Contains("pricing") Then
+            Response.RedirectPermanent(baseUrl & "error/404")
+        End If
+        If uriObject.Contains("product") Then
+            Response.RedirectPermanent(baseUrl & "Home/Products")
+        End If
+        If uriObject.Contains("learning") Then
+            Response.RedirectPermanent(baseUrl & "Home/LearningContent")
+        End If
+        If uriObject.Contains("findyourcentre") Then
+            Response.RedirectPermanent(baseUrl & "FindYourCentre")
         End If
     End Sub
     Private Sub Session_Start(ByVal sender As Object, ByVal e As EventArgs)
